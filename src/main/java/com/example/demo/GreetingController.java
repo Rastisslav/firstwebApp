ef5d9ac78dd0d334ffc06d72809a858ca69c391f
Rastisslav <bolibruch2000@gmail.com>
2022-07-03 19:46:21 +0200

    made working data update

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -8,11 +8,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
-    @GetMapping("/greeting")
+   /* @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+        model.addAttribute("studentName", name);
         return "greeting";
     }
+    */
+
+    @GetMapping("/processForm")
+    public String processForm(){return "greeting"; }
 
     @GetMapping("/helloForm")
     public String helloForm() {
