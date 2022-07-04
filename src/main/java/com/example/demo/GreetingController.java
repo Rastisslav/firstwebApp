7099ced825ed54e555cbe62b814885c08c30c945
Rastisslav <bolibruch2000@gmail.com>
2022-07-04 10:48:16 +0200

    requestparam v controller

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -5,19 +5,29 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class GreetingController {
 
-   /* @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("studentName", name);
+    @GetMapping("/processForm")
+    public String greetings(@RequestParam("studentName") String theName,
+                            Model model) {
+        //model.addAttribute("studentName", name);
+       // String theName = request.getParameter("studentName");
+        theName = theName.toUpperCase();
+        String name2 = theName + " second";
+
+        model.addAttribute("naame",theName);
+        model.addAttribute("name2",name2);
+
         return "greeting";
     }
-    */
 
+/*
     @GetMapping("/processForm")
     public String processForm(){return "greeting"; }
-
+*/
     @GetMapping("/helloForm")
     public String helloForm() {
         return "helloForm";
